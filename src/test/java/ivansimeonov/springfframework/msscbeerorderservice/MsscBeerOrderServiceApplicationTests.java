5e1cf65f23dbb81d26c99fd9fd6e4c7cab86d976
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-26 19:48:01 +0300

    Initial Commit

---
+++ src/test/java/ivansimeonov/springfframework/msscbeerorderservice/MsscBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ivansimeonov.springfframework.msscbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
