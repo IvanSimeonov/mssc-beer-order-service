ffb6d1909f789cbd290d40934ee4dfbcd49bb0f8
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-26 21:27:26 +0300

    Domain classes added

---
+++ src/main/java/ivansimeonov/springfframework/msscbeerorderservice/domain/OrderStatusEnum.java
@@ -0,0 +1,9 @@
+package ivansimeonov.springfframework.msscbeerorderservice.domain;
+
+/**
+ * @Author ivansimeonov
+ * @Date 26.04.22
+ */
+public enum OrderStatusEnum {
+    NEW, READY, PICKED_UP, DECLINED
+}
