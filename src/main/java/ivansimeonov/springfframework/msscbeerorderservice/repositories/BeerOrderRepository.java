c71e5c647feb9b8042013a3dc5b6c31d2c0286a2
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-27 17:22:23 +0300

    Component annotation added to mapper; BeerRepository wrong class import fix; BeerOrderServiceImpl implemented

--- src/main/java/ivansimeonov/springfframework/msscbeerorderservice/repositories/BeerOrderRepository.java
+++ src/main/java/ivansimeonov/springfframework/msscbeerorderservice/repositories/BeerOrderRepository.java
@@ -4,11 +4,11 @@ import ivansimeonov.springfframework.msscbeerorderservice.domain.BeerOrder;
 import ivansimeonov.springfframework.msscbeerorderservice.domain.Customer;
 import ivansimeonov.springfframework.msscbeerorderservice.domain.OrderStatusEnum;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 
 import javax.persistence.LockModeType;
-import java.awt.print.Pageable;
 import java.util.List;
 import java.util.UUID;
 
