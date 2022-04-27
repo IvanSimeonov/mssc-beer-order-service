9dd208f156b63a25d38b17dfbfa369ce97749d13
Ivan Simeonov <nobu.siiw@akros.ch>
2022-04-27 17:26:14 +0300

    Unused imports removed

--- src/main/java/ivansimeonov/springfframework/msscbeerorderservice/services/BeerOrderServiceImpl.java
+++ src/main/java/ivansimeonov/springfframework/msscbeerorderservice/services/BeerOrderServiceImpl.java
@@ -8,13 +8,11 @@ import ivansimeonov.springfframework.msscbeerorderservice.repositories.CustomerR
 import ivansimeonov.springfframework.msscbeerorderservice.web.mappers.BeerOrderMapper;
 import ivansimeonov.springfframework.msscbeerorderservice.web.model.BeerOrderDto;
 import ivansimeonov.springfframework.msscbeerorderservice.web.model.BeerOrderPagedList;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
