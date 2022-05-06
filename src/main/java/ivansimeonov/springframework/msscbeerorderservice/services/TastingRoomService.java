60e07a8dbe1f88075aabbd9613b3c89b9eed896e
Ivan Simeonov <nobu.siiw@akros.ch>
2022-05-06 11:09:36 +0300

    Beer UPC added; Tasting Room added

--- src/main/java/ivansimeonov/springframework/msscbeerorderservice/services/TastingRoomService.java
+++ src/main/java/ivansimeonov/springframework/msscbeerorderservice/services/TastingRoomService.java
@@ -1,5 +1,82 @@
-package ivansimeonov.springframework.msscbeerorderservice.services;/**
+package ivansimeonov.springframework.msscbeerorderservice.services;
+
+import ivansimeonov.springframework.msscbeerorderservice.bootstrap.BeerOrderBootstrap;
+import ivansimeonov.springframework.msscbeerorderservice.domain.Customer;
+import ivansimeonov.springframework.msscbeerorderservice.repositories.BeerOrderRepository;
+import ivansimeonov.springframework.msscbeerorderservice.repositories.CustomerRepository;
+import ivansimeonov.springframework.msscbeerorderservice.web.model.BeerOrderDto;
+import ivansimeonov.springframework.msscbeerorderservice.web.model.BeerOrderLineDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+/**
  *   @Author ivansimeonov
  *   @Date 6.05.22
-*/public class TastingRoomService {
+*/
+@Service
+@Slf4j
+public class TastingRoomService {
+
+    private final CustomerRepository customerRepository;
+    private final BeerOrderService beerOrderService;
+    private final BeerOrderRepository beerOrderRepository;
+    private final List<String> beerUpcs = new ArrayList<>(3);
+
+    public TastingRoomService(CustomerRepository customerRepository, BeerOrderService beerOrderService,
+                              BeerOrderRepository beerOrderRepository) {
+        this.customerRepository = customerRepository;
+        this.beerOrderService = beerOrderService;
+        this.beerOrderRepository = beerOrderRepository;
+
+        beerUpcs.add(BeerOrderBootstrap.BEER_1_UPC);
+        beerUpcs.add(BeerOrderBootstrap.BEER_2_UPC);
+        beerUpcs.add(BeerOrderBootstrap.BEER_3_UPC);
+    }
+
+    @Transactional
+    @Scheduled(fixedRate = 2000) //run every 2 seconds
+    public void placeTastingRoomOrder(){
+
+        List<Customer> customerList = customerRepository.findAllByCustomerNameLike(BeerOrderBootstrap.TASTING_ROOM);
+
+        if (customerList.size() == 1){ //should be just one
+            doPlaceOrder(customerList.get(0));
+        } else {
+            log.error("Too many or too few tasting room customers found");
+
+            customerList.forEach(customer -> log.debug(customer.toString()));
+        }
+    }
+
+    private void doPlaceOrder(Customer customer) {
+        String beerToOrder = getRandomBeerUpc();
+
+        BeerOrderLineDto beerOrderLine = BeerOrderLineDto.builder()
+                .upc(beerToOrder)
+                .orderQuantity(new Random().nextInt(6)) //todo externalize value to property
+                .build();
+
+        List<BeerOrderLineDto> beerOrderLineSet = new ArrayList<>();
+        beerOrderLineSet.add(beerOrderLine);
+
+        BeerOrderDto beerOrder = BeerOrderDto.builder()
+                .customerId(customer.getId())
+                .customerRef(UUID.randomUUID().toString())
+                .beerOrderLines(beerOrderLineSet)
+                .build();
+
+        BeerOrderDto savedOrder = beerOrderService.placeOrder(customer.getId(), beerOrder);
+
+    }
+
+    private String getRandomBeerUpc() {
+        return beerUpcs.get(new Random().nextInt(beerUpcs.size()));
+    }
 }
